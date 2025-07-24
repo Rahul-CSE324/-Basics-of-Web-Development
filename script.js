f935fd96e670b7cc87b4d67258f7e5a29e6f49f1
RAHUL <2004rahuld@gmail.com>
2025-07-24 20:05:19 +0530

    task 1

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleAbout() {
+  const aboutSection = document.getElementById("about");
+  if (aboutSection.style.display === "none") {
+    aboutSection.style.display = "block";
+  } else {
+    aboutSection.style.display = "none";
+  }
+}
